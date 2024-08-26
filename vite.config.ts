f496f70cc1c6ad61eacce0d5f672eb5c0aa68e49
Elena Kara <e.kara@stellarlabs.ai>
2024-08-26 09:53:54 +0300

    refactor: config and export

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,36 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'node:path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
 
 export default defineConfig({
     plugins: [
         react({ tsDecorators: true }),
+        dts({
+            include: ['./src/index.ts'],
+            insertTypesEntry: true,
+            // outDir: ['.'],
+        }),
     ],
     build: {
         copyPublicDir: false,
         lib: {
             entry: path.resolve(__dirname, 'src/index.ts'),
             formats: ['es'],
-            name: 'ui-kit',
-            fileName: (format) => `ui-kit.${format}.js`,
         },
         rollupOptions: {
             external: ['react', 'react-dom', 'react/jsx-runtime', 'ui-kit-core'],
+            input: {
+                index: 'src/index.ts',
+            },
             output: {
+                entryFileNames: '[name].js',
+                dir: 'dist',
                 globals: {
                     react: 'React',
                     'react-dom': 'ReactDOM',
-                    'ui-kit-core': 'ui-kit-core',
+                    'ui-kit-core': 'UiKitCore',
                 },
             },
         },
