7f697a84bed7ccf2ee780260d089ae2445f8caa4
george-zh-2023 <zhuravlev.georgiy02@mail.ru>
2024-09-06 18:03:22 +0300

    feat(components): components

---
+++ src/components/VerticalDivider/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Divider } from 'ui-kit-core'
+
+export class VerticalDivider extends React.Component {
+    static identifier = 'verticalDivider'
+
+    static isDivider = true
+
+    render() {
+        return <Divider vertical />
+    }
+}
