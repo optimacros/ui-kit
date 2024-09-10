4f6143ec0341399167008dcafbcbc07716bd51e5
george-zh-2023 <zhuravlev.georgiy02@mail.ru>
2024-09-10 10:18:40 +0300

    fix(components): removed tree menu

--- src/components/TreeMenu/index.ts
+++ src/components/TreeMenu/index.ts
@@ -1,4 +1,2 @@
-export * from './TreeMenu'
 export { default as BaseMenu } from './BaseMenu'
-export { default as TreeLineMenu } from './TreeLineMenu'
 export { default as CheckBoxMenu } from './CheckBoxMenu'
