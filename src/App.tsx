1079ee2b357efda053058bf137396fcf57f22bd3
feex-s <feex.mail@gmail.com>
2024-09-24 02:42:32 +0300

    clean src

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import type React from 'react'
 
 function App(): React.JSX.Element {
     return (
--- src/components/Menu/index.ts
+++
@@ -1,3 +0,0 @@
-export { Menu } from './Menu'
-export { SubMenuContainer as SubMenu } from './SubMenuContainer'
-export { MenuItem } from './MenuItem'
