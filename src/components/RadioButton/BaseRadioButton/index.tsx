0d3197982aa97a87bd6274abca07dbe04ff7ad99
george-zh-2023 <zhuravlev.georgiy02@mail.ru>
2024-09-24 19:28:43 +0300

    fix(radio): fixed radio styles

--- src/components/RadioButton/BaseRadioButton/index.tsx
+++ src/components/RadioButton/BaseRadioButton/index.tsx
@@ -2,17 +2,12 @@ import React from 'react'
 
 import Radio, { RadioProps } from './Radio'
 import RadioButtonComponent from './RadioButton'
-import { mergeStyles } from '../../../utils'
-
-import theme from './radioButtonTheme.module.css'
 
 const RadioButton: React.FC<RadioProps> = props => (
+    // @ts-ignore
     <RadioButtonComponent
         {...props}
         Radio={Radio}
-        theme={props.theme
-            ? mergeStyles(theme, props.theme)
-            : theme}
     />
 )
 
--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -4,6 +4,7 @@ import { RadioButton as Base } from './BaseRadioButton'
 import { RadioProps } from './BaseRadioButton/Radio'
 import { mergeStyles } from '../../utils'
 
+import baseTheme from './BaseRadioButton/radioButtonTheme.module.css'
 import styles from './RadioButton.module.css'
 
 export const RadioButton: React.FC<RadioProps> = props => {
@@ -13,7 +14,7 @@ export const RadioButton: React.FC<RadioProps> = props => {
     return (
         <Base
             label={props.label || props.placeholder}
-            theme={mixTheme}
+            theme={mergeStyles(baseTheme, mixTheme)}
             {...otherProps}
         />
     )
