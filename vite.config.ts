bbc6d8bc44a261780fea7dd999d1bb2f4bf7e896
feex-s <feex.mail@gmail.com>
2024-09-24 23:04:14 +0300

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,13 @@ import postcssPresetEnv from 'postcss-preset-env'
 // @ts-ignore
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+// TODO https://github.com/gxmari007/vite-plugin-eslint/issues/84
 // @ts-ignore
 import eslint from 'vite-plugin-eslint'
 import { libInjectCss } from 'vite-plugin-lib-inject-css'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         libInjectCss(),
@@ -24,6 +27,7 @@ export default defineConfig({
             include: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
             exclude: [],
         }),
+        tsconfigPaths(),
         dts({
             insertTypesEntry: true,
         }),
@@ -35,6 +39,7 @@ export default defineConfig({
                 const componentName = filename
                     .split('/')
                     .pop()
+
                 const hash = crypto
                     .createHash('md5')
                     .update(css)
@@ -72,21 +77,36 @@ export default defineConfig({
         },
         rollupOptions: {
             external: ['react', 'react-dom', 'react/jsx-runtime', 'ui-kit-core', 'lodash'],
-            input: Object.fromEntries(
-                glob.sync(
-                    './src/**/*.{ts,tsx}',
-                ).map(file => [
-                    // The name of the entry point
-                    // src/components/nested/foo.ts becomes nested/foo
-                    path.relative(
-                        'src',
-                        file.slice(0, file.length - path.extname(file).length),
+            input: {
+                ...(Object.fromEntries(
+                    glob.sync(
+                        './src/**/*.{ts,tsx}',
+                    ).map(file => [
+                        // The name of the entry point
+                        // src/components/nested/foo.ts becomes nested/foo
+                        path.relative(
+                            'src',
+                            file.slice(0, file.length - path.extname(file).length),
+                        ),
+                        // The absolute path to the entry file
+                        // src/components/nested/foo.ts becomes /project/src/components/nested/foo.ts
+                        fileURLToPath(new URL(file, import.meta.url)),
+                    ]),
+                )),
+                ...(Object.fromEntries(
+                    glob.sync(
+                        './src/fonts/*.css',
+                    ).map(file => [
+                        path.relative(
+                            'src',
+                            file.slice(0, file.length - path.extname(file).length),
+                        ),
+                        fileURLToPath(new URL(file, import.meta.url)),
+                    ],
                     ),
-                    // The absolute path to the entry file
-                    // src/components/nested/foo.ts becomes /project/src/components/nested/foo.ts
-                    fileURLToPath(new URL(file, import.meta.url)),
-                ]),
-            ),
+                )),
+            },
+
             output: {
                 chunkFileNames: (chunkInfo) => {
                     switch (chunkInfo.name) {
@@ -96,7 +116,13 @@ export default defineConfig({
                             return 'chunks/[name].js'
                     }
                 },
-                assetFileNames: 'assets/index[extname]',
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name && assetInfo.name.includes('fonts.css')) {
+                        return '[name][extname]'
+                    }
+
+                    return 'assets/index[extname]'
+                },
                 entryFileNames: '[name].js',
                 dir: 'dist',
                 globals: {
@@ -106,5 +132,4 @@ export default defineConfig({
             },
         },
     },
-
 })
