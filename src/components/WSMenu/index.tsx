7402404f30d585592018a51cf9ee296250432381
george-zh-2023 <zhuravlev.georgiy02@mail.ru>
2024-09-25 15:43:42 +0300

    fix(menu-item): fix

--- src/components/WSMenu/index.tsx
+++ src/components/WSMenu/index.tsx
@@ -5,23 +5,29 @@ interface Props {
     title: string;
     key: string;
     label?: string;
+    children: React.ReactNode;
 }
+
 export const WSSubMenu: React.FC<Props> = props => {
+    const { label, title, ...restProps } = props
+
     return (
         <BaseSubMenu
-            {...props}
-            title={props.label || props.title}
+            {...restProps}
+            title={label || title}
         />
     )
 }
 
 export const WSMenuItem = (props: Props): React.JSX.Element => {
-    const { label, title, ...restProps } = props
+    const { label, title, children, ...restProps } = props
 
     return (
         <BaseMenuItem
             {...restProps}
             title={label || title}
-        />
+        >
+            {label || title || children}
+        </BaseMenuItem>
     )
 }
