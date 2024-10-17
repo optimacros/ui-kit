cd9e949c2c37b487278812c8fc3d319070d4dde1
george-zh-2023 <zhuravlev.georgiy02@mail.ru>
2024-10-17 14:10:08 +0300

    feat(RadioGroup)): moved to core

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,52 +1,10 @@
 export { default as InputMask } from 'react-input-mask'
-export {
-    Tooltip,
-    Button,
-    Chip,
-    Counter,
-    Contents,
-    HorizontalDivider,
-    VerticalDivider,
-    Divider,
-    SearchInput,
-    Dropdown,
-    FontIcon,
-    IconButton,
-    Input,
-    LayoutFixedContainer,
-    Loader,
-    Menu,
-    MenuItem,
-    SubMenu,
-    MenuDivider,
-    MenuItemGroup,
-    TextArea,
-    Toolbar,
-    ButtonMenu,
-    Checkbox,
-    Icon,
-    Navigation,
-    Tab,
-    TabHeader,
-    Tabs,
-    SelectBox,
-    DownloadIframe,
-    Favourite,
-    FileInput,
-    Layout,
-    Modal,
-    ProgressBar,
-    RadioButton,
-    Snackbar,
-    TabContent,
-    TabsContainer,
-} from 'ui-kit-core'
+export * from 'ui-kit-core'
 export * from './Calendar'
 export * from './ColorPicker'
 export * from './MarkdownEditor'
 export * from './SliderScale'
 export * from './ViewMenu'
-export * from './RadioGroup'
 export * from './ProgressBars'
 export * from './LocalLoader'
 export * from './Kanban'
