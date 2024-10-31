b5737a99bd59d1272d727a63679a21d8ffa52077
george-zh-2023 <zhuravlev.georgiy02@mail.ru>
2024-10-31 14:27:08 +0300

    chore(release): publish
     - @optimacros/ui-kit-core@1.1.6
     - @optimacros/ui-kit-example@0.0.2
     - @optimacros/ui-kit-layout@0.0.4
     - @optimacros/ui-kit@1.0.7
     - @optimacros/themes@0.0.3

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,19 +3,15 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
     entry: ['src/index.ts'],
-    splitting: true,
-    sourcemap: true,
+    sourcemap: false,
     clean: true,
     treeshake: true,
-    metafile: true,
     target: 'es2020',
     dts: true,
     platform: 'browser',
     outDir: 'dist',
-    //@ts-ignore
-    // onSuccess() {
-    //     const pkg = require('./package.json');
-    //     console.log(pkg);
-    //     return exec('cp -a package.json dist');
-    // },
+    bundle: true,
+    loader: {
+        '.css': 'local-css',
+    },
 });
