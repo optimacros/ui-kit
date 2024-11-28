13adc5fecfbe13f63124312d1291109292ad8910
Front-start <a.chepusov@optisoft.net>
2024-11-28 13:58:19 +0300

    fix(modal): remove close icon

--- packages/core/src/components/ModalV2/components/Header.tsx
+++ packages/core/src/components/ModalV2/components/Header.tsx
@@ -1,26 +1,14 @@
 import { forward, styled } from '@optimacros/ui-kit-store';
 import { tw } from '@optimacros/ui-kit-utils';
 import React from 'react';
-import { useApi } from './context';
-import { Icon } from '../../Icon';
-import { ICONS_MAP } from '@optimacros/themes';
 
 export const headerClassName = tw`relative pl-6 pr-14 py-3.5 flex flex-row items-center w-full box-border`;
-export const iconClassName = tw`absolute size-6 text-[var(--Checkbox-color)] hover:text-[var(--color-primary)] cursor-pointer top-4 right-4`;
 
 export const Header = forward<React.PropsWithChildren, 'div'>(
     ({ children, ...rest }, ref) => {
-        const api = useApi();
-
         return (
             <styled.div className={headerClassName} {...rest} ref={ref} data-part="header">
                 {children}
-
-                <Icon
-                    {...api.getCloseTriggerProps()}
-                    value={ICONS_MAP.close}
-                    className={iconClassName}
-                />
             </styled.div>
         );
     },
