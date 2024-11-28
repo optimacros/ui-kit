967c699408e9c9f68e89a36526135a335fdf990f
Front-start <a.chepusov@optisoft.net>
2024-11-28 14:04:13 +0300

    fix(modal): rename class

--- packages/core/src/components/ModalV2/components/Root.tsx
+++ packages/core/src/components/ModalV2/components/Root.tsx
@@ -8,7 +8,7 @@ import { tw } from '@optimacros/ui-kit-utils';
 export const backdropClassName = tw`fixed z-[var(--z-dialog-backdrop)] inset-0 bg-[var(--Modal-overlay-bg)]`;
 export const positionerClassName = tw`fixed z-[var(--z-dialog-backdrop)] inset-0 flex items-center justify-center h-screen w-screen`;
 export const contentClassName = tw`flex flex-col items-start rounded-[var(--border-radius)] bg-[var(--Modal-bg)]
-border-1 border-solid border-[var(--color-divider)] max-w-[80vw] max-h-[80vh]`;
+border-1 border-solid border-divider max-w-[80vw] max-h-[80vh]`;
 
 interface Props extends React.PropsWithChildren, Partial<Context> {
     onRequestClose?: () => void;
