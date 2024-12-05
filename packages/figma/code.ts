d8678edaed2d171a8250d9575d3a0b75d5ce18f5
george-zh-2023 <zhuravlev.georgiy02@mail.ru>
2024-12-05 04:53:46 +0300

    feat(figma): upd

--- packages/figma/code.ts
+++ packages/figma/code.ts
@@ -1,6 +1,6 @@
 //@ts-nocheck
-
 import tinycolor from 'tinycolor2';
+import { componentNames } from './component-names';
 //biome-ignore lint: wait
 figma.showUI(__html__, { themeColors: true, height: 400, width: 350 });
 
@@ -25,6 +25,10 @@ const transformSize = (name: string, v: number) => (isSize(name) ? convertNumber
 const getAliasValue = (v: any, arr: Array<Variable>) => {
     //@ts-ignore
     const findValue = (v: any) => {
+        if (!v) {
+            return '';
+        }
+
         if (v.type === 'VARIABLE_ALIAS') {
             return findValue(arr.find((val) => val.id == v.id));
         }
@@ -58,67 +62,156 @@ const mapByMode = (vars: Variable[], mapper: (variable: Variable, value: any) =>
     return result;
 };
 
-const exportTheme = () => {
-    //biome-ignore lint: wait
-    const colors = figma.variables.getLocalVariablesAsync('COLOR').then((vars) => {
-        //@ts-ignore
-        const transformRGBA = (v) => {
-            return Object.keys(v).reduce((acc, key) => {
-                return {
-                    ...acc,
-                    [key]: transformColor(v[key]),
-                };
-            }, {});
+const transformRGBA = (v) => {
+    return Object.keys(v).reduce((acc, key) => {
+        return {
+            ...acc,
+            [key]: transformColor(v[key]),
         };
+    }, {});
+};
 
-        return mapByMode(
-            vars,
-            ({ name }, v) =>
-                `${transformVarName(name)}: ${tinycolor(transformRGBA(v)).toHexString()}`,
-        );
-    });
-
-    //biome-ignore lint: wait
-    const sizes = figma.variables.getLocalVariablesAsync('FLOAT').then((vars) => {
-        //@ts-ignore
-        return vars.map(({ valuesByMode, name }) =>
-            Object.values(valuesByMode).map(
-                (v) => `${transformVarName(name)}: ${transformSize(name, v)}`,
-            ),
-        );
-    });
-
-    //biome-ignore lint: wait
-    const other = figma.variables.getLocalVariablesAsync('STRING').then((vars) => {
-        //@ts-ignore
-        return vars.map(({ valuesByMode, name }) =>
-            Object.values(valuesByMode).map((v) => `${transformVarName(name)}: ${v}`),
-        );
-    });
-
-    Promise.all([colors, sizes, other]).then((v) => {
-        const data =
-            'data:text/css;charset=utf-8,' +
-            encodeURIComponent(
-                `:root{ ${v
-                    .filter((arr) => arr.length > 0)
-                    .flat()
-                    .join(`;`)}; }`,
-            );
-
-        //biome-ignore lint: wait
-        figma.ui.postMessage({
-            type: 'download',
-            data: {
-                filename: 'theme.css',
-                data,
-            },
-        });
+const transformVars = (vars) => {
+    return mapByMode(vars, ({ name, resolvedType }, v) => {
+        switch (resolvedType) {
+            case 'COLOR': {
+                return `${transformVarName(name)}: ${tinycolor(transformRGBA(v)).toHexString()}`;
+            }
+            case 'FLOAT': {
+                return `${transformVarName(name)}: ${transformSize(name, v)}`;
+            }
+            default: {
+                return `${transformVarName(name)}: ${v}`;
+            }
+        }
     });
 };
 
+const getThemeVariables = () => {
+    //biome-ignore lint: wait
+    const vars = figma.variables.getLocalVariablesAsync();
+
+    return vars.then((resolvedVars) => {
+        return transformVars(resolvedVars);
+    });
+};
+
+const createVariables = ({
+    levels,
+    states,
+    onCombined = (v) => v,
+}: { levels: string[]; states: string[]; onCombined: (combined: string[]) => string[] }) => {
+    //TODO add ui
+    const combine = onCombined(levels.map((l) => [...states.map((s) => `${l}.${s}`), l]).flat());
+
+    //biome-ignore lint: wait
+    const collection = figma.variables.getVariableCollectionByIdAsync('VariableCollectionId:267:2');
+
+    return collection
+        .then((c) => {
+            return combine.map((v) =>
+                //biome-ignore lint: wait
+                figma.variables.createVariable(v.replaceAll('.', '/'), c, 'COLOR'),
+            );
+        })
+        .catch((e) => {
+            console.error(e);
+        });
+};
+
+const cssStateKey = 'css-element-state';
+
 const msgRecord = {
-    exportTheme,
+    exportTheme: () => {
+        const themeVars = getThemeVariables();
+
+        themeVars.then((v) => {
+            const data =
+                'data:text/css;charset=utf-8,' + encodeURIComponent(`:root{ ${v.join(`;`)}; }`);
+
+            //biome-ignore lint: wait
+            figma.ui.postMessage({
+                type: 'download',
+                data: {
+                    filename: 'theme.css',
+                    data,
+                },
+            });
+        });
+    },
+    createVariables: () => {
+        createVariables({
+            levels: ['primary', 'secondary', 'neutral', 'accent', 'warning', 'success', 'error']
+                .map((v) => [v, `on.${v}`])
+                .flat(),
+            states: ['hover', 'focus', 'active'],
+            onCombined(combined) {
+                return combined
+                    .map((v) => {
+                        return componentNames.map((name) => `color.${name}.${v}`);
+                    })
+                    .flat();
+            },
+        });
+    },
+    exportComponents: () => {
+        //biome-ignore lint: wait
+        const vars = figma.variables.getLocalVariablesAsync();
+
+        vars.then((allVars) => {
+            //biome-ignore lint: wait
+            const components = figma.currentPage
+                .findAllWithCriteria({ types: ['COMPONENT_SET'] })
+                .map((v) => {
+                    const componentName = v.name.replaceAll(' ', '');
+                    const componentRegexp = new RegExp(
+                        `((\\w*)\\/${componentName}\\/(.*))|(^${componentName}\\/(.*))`,
+                        'g',
+                    );
+
+                    const componentVars = allVars.filter((variable) =>
+                        variable.name.match(componentRegexp),
+                    );
+
+                    const componentVarsCss = `:root{${transformVars(componentVars).join(`;`)}}`;
+
+                    return v.children.map((component) => {
+                        const cssState = component.variantProperties[cssStateKey];
+
+                        const selectorBase = Object.keys(component.variantProperties)
+                            .filter((key) => key !== cssStateKey)
+                            .map((key) => {
+                                return `[${key}=${component.variantProperties[key]}]`;
+                            })
+                            .join('');
+
+                        const selector = cssState
+                            ? `${selectorBase}:${cssState}, ${selectorBase}[data-${cssState}=true]`
+                            : selectorBase;
+
+                        component.getCSSAsync().then((css) => {
+                            // everything is fine
+                            // to get text color we need to parse children and get their color, maybe some naming conventions there
+                            const text = component.children.find((c) => c.name === 'data-text');
+                            text &&
+                                text.getCSSAsync().then((textCss) => {
+                                    const res = {
+                                        ...css,
+                                        color: textCss.color,
+                                    };
+                                    // console.log(res);
+                                    return {
+                                        ...css,
+                                        color: textCss.color,
+                                    };
+                                });
+                        });
+
+                        return component;
+                    });
+                });
+        });
+    },
 };
 
 //biome-ignore lint: wait
