3794acedf3bf7bc69cfda944e6c8d8f8b17c61df
Front-start <a.chepusov@optisoft.net>
2024-12-12 20:25:50 +0300

    feat(slider): add docs, stories

--- packages/core/src/components/Slider/src/Slider.stories.tsx
+++ packages/core/src/components/Slider/src/Slider.stories.tsx
@@ -1,19 +1,24 @@
 import { ArgTypes, Meta } from '@storybook/react';
 import { Slider } from './index';
 import { Button } from '@optimacros-ui/button';
+import { useState } from 'react';
+import { ValueChangeDetails } from '@zag-js/slider';
 
 const argTypes: Partial<ArgTypes> = {
     value: {
         control: false,
-        description: 'Current slider value',
-        required: true,
+        description: 'Current slider value(s)',
         table: { type: { summary: 'number[]' } },
     },
     onValueChange: {
         control: false,
         description: 'Callback function that is called on value change',
         table: { type: { summary: '({value}) => void' } },
-        required: true,
+    },
+    onValueChangeEnd: {
+        control: false,
+        description: 'Callback function that is called on value change',
+        table: { type: { summary: '({value}) => void' } },
     },
     min: {
         control: 'number',
@@ -25,8 +30,19 @@ const argTypes: Partial<ArgTypes> = {
         description: 'Max slider value',
         table: { defaultValue: { summary: '100' } },
     },
+    step: {
+        control: 'number',
+        description: 'The step value of the slider',
+        table: { defaultValue: { summary: '1' } },
+    },
+    minStepsBetweenThumbs: {
+        control: 'number',
+        description: 'The minimum permitted steps between multiple thumbs.',
+        table: { defaultValue: { summary: '0' } },
+    },
     disabled: {
         control: 'boolean',
+        description: 'Whether the slider is disabled',
         table: { defaultValue: { summary: 'false' } },
     },
 };
@@ -39,8 +55,14 @@ const meta: Meta<typeof Slider> = {
 export default meta;
 
 export const Basic = () => {
+    const [value, setValue] = useState([67]);
+
+    const handleChange = (details: ValueChangeDetails) => {
+        setValue(details.value);
+    };
+
     return (
-        <Slider.RootProvider value={[67]}>
+        <Slider.RootProvider value={value} onValueChange={handleChange} invalid>
             {(api) => (
                 <>
                     <div style={{ marginBottom: 50 }}>
@@ -57,59 +79,4 @@ export const Basic = () => {
     );
 };
 
-export const Range = () => {
-    return (
-        <Slider.RootProvider value={[12, 34]}>
-            {(api) => (
-                <>
-                    <div style={{ marginBottom: 50 }}>
-                        <Button onClick={() => api.setValue([12, 34])}>reset</Button>
-                    </div>
-
-                    <Slider.Root>
-                        <Slider.Label>Quantity</Slider.Label>
-                        <Slider.Output />
-                    </Slider.Root>
-                </>
-            )}
-        </Slider.RootProvider>
-    );
-};
-
-export const Disabled = () => {
-    return (
-        <Slider.RootProvider value={[12, 34]} disabled>
-            {(api) => (
-                <>
-                    <div style={{ marginBottom: 50 }}>
-                        <Button onClick={() => api.setValue([12, 34])}>reset</Button>
-                    </div>
-
-                    <Slider.Root>
-                        <Slider.Label>Quantity</Slider.Label>
-                        <Slider.Output />
-                    </Slider.Root>
-                </>
-            )}
-        </Slider.RootProvider>
-    );
-};
-
-export const CustomMinMax = () => {
-    return (
-        <Slider.RootProvider value={[12, 34]} min={-100} max={50}>
-            {(api) => (
-                <>
-                    <div style={{ marginBottom: 50 }}>
-                        <Button onClick={() => api.setValue([12, 34])}>reset</Button>
-                    </div>
-
-                    <Slider.Root>
-                        <Slider.Label>Quantity</Slider.Label>
-                        <Slider.Output />
-                    </Slider.Root>
-                </>
-            )}
-        </Slider.RootProvider>
-    );
-};
+export { Range, Disabled, CustomMinMax, Step, MinStep } from './stories';
