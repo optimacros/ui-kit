684687462b2a2f4f6f8d7075e2d047dbe38653ea
Front-start <a.chepusov@optisoft.net>
2024-12-12 21:32:30 +0300

    fix(storybook): fix show code button bug

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,7 @@
 import { UiCoreContextWrapper } from '../packages/main/src';
 import { Preview } from '@storybook/react';
 import iconsSrc from '../packages/themes/src/assets/icons/optimacros/sprite/index.svg';
+import { DocsContainer } from '@storybook/blocks';
 
 const preview: Preview = {
     parameters: {
@@ -9,6 +10,19 @@ const preview: Preview = {
             matchers: {
                 color: /(background|color)$/i,
                 date: /Date$/,
+            },
+        },
+        /** this section is here because of
+         * https://github.com/storybookjs/storybook/issues/26505
+         */
+        docs: {
+            container: DocsContainer,
+            source: {
+                type: 'dynamic',
+                excludeDecorators: true,
+            },
+            controls: {
+                sort: 'alpha',
             },
         },
     },
