14bc315c560566cba0b068d42e733418c80bc366
JuliaJu93 <kashlewa.ju93@gmail.com>
2024-12-13 23:19:12 +0300

    chore(chip): tailwind to css
    # Conflicts:
    #	packages/core/src/components/Chip/src/Chip.tsx
    #	packages/core/src/components/Chip/src/index.css

--- packages/core/src/components/Chip/src/Chip.tsx
+++ packages/core/src/components/Chip/src/Chip.tsx
@@ -1,9 +1,9 @@
 import { forward, styled } from '@optimacros-ui/store';
 
 export const Root = forward<{}, 'div'>((props, ref) => (
-    <styled.div {...props} ref={ref} data-scope="chip" data-part="root" />
+    <styled.div {...props} ref={ref} data-scope="chip" data-part="root" className={rootClassName} />
 ));
 
-export const Icon = forward<{}, 'div'>((props, ref) => (
-    <styled.div role="button" {...props} ref={ref} data-scope="chip" data-part="icon" />
+export const Icon = forward<{}, 'span'>((props, ref) => (
+    <styled.span role="button" {...props} ref={ref} data-scope="chip" data-part="icon" />
 ));
