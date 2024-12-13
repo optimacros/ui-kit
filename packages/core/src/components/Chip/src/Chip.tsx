b3c7af69ebd4ad7bf8d6bc038135be9938e135e2
JuliaJu93 <kashlewa.ju93@gmail.com>
2024-12-14 00:13:04 +0300

    chore(chip): added stories

--- packages/core/src/components/Chip/src/Chip.tsx
+++ packages/core/src/components/Chip/src/Chip.tsx
@@ -7,3 +7,7 @@ export const Root = forward<{}, 'div'>((props, ref) => (
 export const Icon = forward<{}, 'span'>((props, ref) => (
     <styled.span role="button" {...props} ref={ref} data-scope="chip" data-part="icon" />
 ));
+
+export const Avatar = forward<{}, 'img'>((props, ref) => (
+    <styled.img role="button" {...props} ref={ref} data-scope="chip" data-part="avatar" />
+));
