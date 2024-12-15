81f90965b2364956c6e6c49dd15cf692a86212e0
Front-start <a.chepusov@optisoft.net>
2024-12-15 19:10:33 +0300

    chore(storybook): remove source code fix

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,6 @@
 import { UiCoreContextWrapper } from '../packages/core/src';
 import { Preview } from '@storybook/react';
 import iconsSrc from '../packages/themes/src/assets/icons/optimacros/sprite/index.svg';
-import { DocsContainer } from '@storybook/blocks';
 
 const preview: Preview = {
     parameters: {
@@ -10,19 +9,6 @@ const preview: Preview = {
             matchers: {
                 color: /(background|color)$/i,
                 date: /Date$/,
-            },
-        },
-        /** this section is here because of
-         * https://github.com/storybookjs/storybook/issues/26505
-         */
-        docs: {
-            container: DocsContainer,
-            source: {
-                type: 'dynamic',
-                excludeDecorators: true,
-            },
-            controls: {
-                sort: 'alpha',
             },
         },
     },
