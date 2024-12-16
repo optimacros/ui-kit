e0b0600978fbd84ef414349269573471f1c270ed
JuliaJu93 <kashlewa.ju93@gmail.com>
2024-12-16 19:03:01 +0300

    chore(divider): improved stories

--- packages/core/src/components/Divider/src/Divider.stories.tsx
+++ packages/core/src/components/Divider/src/Divider.stories.tsx
@@ -1,12 +1,15 @@
-import { Divider } from './Divider';
+import { Flex } from '@optimacros-ui/flex';
+import { Text } from '@optimacros-ui/text';
+import { Icon } from '@optimacros-ui/icon';
 import { Orientation } from '@optimacros-ui/utils';
+import { Divider } from './Divider';
 
 export default {
     title: 'UI Kit core/Divider',
     component: Divider,
     tags: ['autodocs'],
     argTypes: {
-        variant: {
+        orientation: {
             control: { type: 'radio' },
             options: Orientation,
         },
@@ -18,9 +21,27 @@ export const Base = (props) => {
 };
 
 export const Vertical = (props) => {
-    return <Divider orientation={Orientation.Vertical} {...props} />;
+    return (
+        <Flex gap="3" align="center">
+            <Icon value="format_align_left" />
+            <Icon value="format_align_center" />
+            <Icon value="format_align_right" />
+            <Divider orientation={Orientation.Vertical} {...props} />
+            <Icon value="format_bold" />
+        </Flex>
+    );
 };
 
 export const Horizonatal = (props) => {
-    return <Divider orientation={Orientation.Horizontal} {...props} />;
+    return (
+        <div style={{ width: '200px' }}>
+            <Flex align="center" direction="column">
+                <Text.Paragraph>Full width variant below</Text.Paragraph>
+                <Divider orientation={Orientation.Horizontal} {...props} />
+                <Text.Paragraph>Inset variant below</Text.Paragraph>
+                <Divider orientation={Orientation.Horizontal} {...props} />
+                <Text.Paragraph>Middle variant below</Text.Paragraph>
+            </Flex>
+        </div>
+    );
 };
