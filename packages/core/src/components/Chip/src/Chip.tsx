709df6039342dbd10d5faf13058f15f8c7daf544
yyusupova <y.yusupova@optisoft.net>
2024-12-17 11:08:04 +0000

    Ukf-173 Chip

--- packages/core/src/components/Chip/src/Chip.tsx
+++ packages/core/src/components/Chip/src/Chip.tsx
@@ -4,6 +4,10 @@ export const Root = forward<{}, 'div'>((props, ref) => (
     <styled.div {...props} ref={ref} data-scope="chip" data-part="root" />
 ));
 
-export const Icon = forward<{}, 'div'>((props, ref) => (
-    <styled.div role="button" {...props} ref={ref} data-scope="chip" data-part="icon" />
+export const Icon = forward<{}, 'span'>((props, ref) => (
+    <styled.span role="button" {...props} ref={ref} data-scope="chip" data-part="icon" />
+));
+
+export const Avatar = forward<{}, 'img'>((props, ref) => (
+    <styled.img role="button" {...props} ref={ref} data-scope="chip" data-part="avatar" />
 ));
