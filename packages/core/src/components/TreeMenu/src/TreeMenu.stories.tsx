212b99bcfc2448b021d00c32aed1752f05319a56
JuliaJu93 <kashlewa.ju93@gmail.com>
2024-12-18 18:36:12 +0300

    chore(tree): improved stories
    # Conflicts:
    #	package-lock.json

--- packages/core/src/components/TreeMenu/src/TreeMenu.stories.tsx
+++ packages/core/src/components/TreeMenu/src/TreeMenu.stories.tsx
@@ -13,6 +13,48 @@ export default {
     title: 'UI Kit core/TreeMenu',
     component: TreeMenu.Root,
     tags: ['autodocs'],
+    argTypes: {
+        menuItems: {
+            control: { type: 'object' },
+            description: 'Tree menu items data structure',
+        },
+        selectionMode: {
+            control: { type: 'radio' },
+            options: ['single', 'multiple'],
+            defaultValue: 'single',
+            description: 'Selection mode for tree items',
+        },
+        selectedValue: {
+            control: { type: 'array' },
+            description: 'Array of selected item IDs',
+        },
+        expandedValue: {
+            control: { type: 'array' },
+            description: 'Array of expanded item IDs',
+        },
+        onExpandedChange: {
+            action: 'expandedChanged',
+            description: 'Callback fired when expansion state changes',
+        },
+        onSelectedChange: {
+            action: 'selectedChanged',
+            description: 'Callback fired when selection changes',
+        },
+        positioning: {
+            control: { type: 'select' },
+            options: ['popper', 'portal', 'inline'],
+            defaultValue: 'popper',
+            description: 'Positioning strategy for the menu',
+        },
+        open: {
+            control: { type: 'boolean' },
+            description: 'Controls the open state of the menu',
+        },
+        onOpenChange: {
+            action: 'openChanged',
+            description: 'Callback fired when open state changes',
+        },
+    },
     decorators: [
         (Story) => (
             <Wrapper>
@@ -29,7 +71,84 @@ export const Basic = () => {
         <TreeMenu.Root menuItems={menuItems}>
             <TreeMenu.PortalRoot>
                 <TreeMenu.Trigger asChild>
-                    <Button variant="bordered">Click me</Button>
+                    <Button variant="bordered">Menu</Button>
+                </TreeMenu.Trigger>
+                <TreeMenu.Positioner>
+                    <TreeMenu.Content>
+                        <TreeMenu.Tree>
+                            {menuItems.rootNode.children.map((node, index) => (
+                                <TreeMenu.TreeNode key={node.id} node={node} indexPath={[index]}>
+                                    <Icon value="chevron_right" />
+                                </TreeMenu.TreeNode>
+                            ))}
+                        </TreeMenu.Tree>
+                    </TreeMenu.Content>
+                </TreeMenu.Positioner>
+            </TreeMenu.PortalRoot>
+        </TreeMenu.Root>
+    );
+};
+
+export const WithIcons = () => {
+    return (
+        <TreeMenu.Root menuItems={menuItems}>
+            <TreeMenu.PortalRoot>
+                <TreeMenu.Trigger asChild>
+                    <Button variant="bordered">Menu</Button>
+                </TreeMenu.Trigger>
+                <TreeMenu.Positioner>
+                    <TreeMenu.Content>
+                        <TreeMenu.Tree>
+                            {menuItems.rootNode.children.map((node, index) => (
+                                <TreeMenu.TreeNode key={node.id} node={node} indexPath={[index]}>
+                                    <Icon value="chevron_right" />
+                                    <Icon value="folder_open" />
+                                    <Icon value="description" />
+                                </TreeMenu.TreeNode>
+                            ))}
+                        </TreeMenu.Tree>
+                    </TreeMenu.Content>
+                </TreeMenu.Positioner>
+            </TreeMenu.PortalRoot>
+        </TreeMenu.Root>
+    );
+};
+
+export const Multiple = () => {
+    return (
+        <TreeMenu.Root menuItems={menuItems} selectionMode="multiple">
+            <TreeMenu.PortalRoot>
+                <TreeMenu.Trigger asChild>
+                    <Button variant="bordered">Menu</Button>
+                </TreeMenu.Trigger>
+                <TreeMenu.Positioner>
+                    <TreeMenu.Content>
+                        <TreeMenu.Tree>
+                            {menuItems.rootNode.children.map((node, index) => (
+                                <TreeMenu.TreeNode key={node.id} node={node} indexPath={[index]}>
+                                    <Icon value="chevron_right" />
+                                    <Icon value="folder_open" />
+                                    <Icon value="description" />
+                                </TreeMenu.TreeNode>
+                            ))}
+                        </TreeMenu.Tree>
+                    </TreeMenu.Content>
+                </TreeMenu.Positioner>
+            </TreeMenu.PortalRoot>
+        </TreeMenu.Root>
+    );
+};
+
+export const selectedValue = () => {
+    return (
+        <TreeMenu.Root
+            menuItems={menuItems}
+            selectedValue={['selected']}
+            expandedValue={['parent-selected']}
+        >
+            <TreeMenu.PortalRoot>
+                <TreeMenu.Trigger asChild>
+                    <Button variant="bordered">Menu</Button>
                 </TreeMenu.Trigger>
                 <TreeMenu.Positioner>
                     <TreeMenu.Content>
