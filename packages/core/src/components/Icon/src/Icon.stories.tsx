5a58d862a10b4503b1c4fb8432fa81d7488f7648
yyusupova <y.yusupova@optisoft.net>
2024-12-19 20:41:16 +0000

    UKF-211 - Icon

--- packages/core/src/components/Icon/src/Icon.stories.tsx
+++ packages/core/src/components/Icon/src/Icon.stories.tsx
@@ -1,59 +1,124 @@
-import { ArgTypes, Meta, StoryObj } from '@storybook/react';
+import { ArgTypes, Meta } from '@storybook/react';
+import { Tooltip } from '@optimacros-ui/core';
+import { ICONS_MAP } from '@optimacros-ui/themes';
+import { Grid } from '@optimacros-ui/grid';
+import { Flex } from '@optimacros-ui/flex';
+import { Text } from '@optimacros-ui/text';
+import { Popover } from '@optimacros-ui/popover';
+import { Toolbar } from '@optimacros-ui/toolbar';
+import { Button } from '@optimacros-ui/button';
+import { IconButton } from '@optimacros-ui/icon-button';
 
 import { Icon } from './index';
 
 const argTypes: Partial<ArgTypes> = {
-    name: {
+    value: {
         control: 'text',
-        description: 'Value of the icon. Using `FontIcon` if value string. Or `div` if value node.',
-    },
-    title: {
-        control: 'text',
-        description: 'Icon description, visible on icon hover.',
-    },
-    alt: {
-        control: 'text',
-        description: 'The text used to set the `aria-label` attribute.',
+        description: 'Icon name',
     },
     style: {
         control: 'object',
         description: 'Add styles to component.',
     },
-    className: {
-        table: { disable: true },
-    },
-    onClick: {
-        table: { disable: true },
-    },
 };
 
-const meta: Meta<typeof Icon> = {
+export default {
     title: 'UI Kit core/Icon',
     component: Icon,
+    tags: ['autodocs'],
     argTypes,
+} as Meta;
+
+export const Basic = () => {
+    const onClickHandler = (icon: keyof typeof ICONS_MAP) => {
+        navigator.clipboard.writeText(icon);
+    };
+
+    return (
+        <div style={{ fontSize: '32px' }}>
+            <Text.Paragraph style={{ margin: '2rem', opacity: '60%' }}>
+                The icon name is copied when you click on it
+            </Text.Paragraph>
+            <Grid.Root cols="6" gap="16">
+                {Object.values(ICONS_MAP).map((icon) => (
+                    <div
+                        key={icon}
+                        onClick={() => onClickHandler(icon)}
+                        style={{
+                            border: '1px solid transparent',
+                            borderRadius: '4px',
+                            padding: '8px',
+                            cursor: 'pointer',
+                            transition: 'border-color 0.2s ease',
+                        }}
+                        onMouseOver={(e) => {
+                            e.currentTarget.style.borderColor = '#007bff';
+                        }}
+                        onMouseOut={(e) => {
+                            e.currentTarget.style.borderColor = 'transparent';
+                        }}
+                    >
+                        <Flex direction="column" gap="4" align="center">
+                            <Icon value={icon} />
+                            <div
+                                style={{
+                                    fontSize: '16px',
+                                    opacity: '60%',
+                                    textAlign: 'center',
+                                }}
+                            >
+                                {icon}
+                            </div>
+                        </Flex>
+                    </div>
+                ))}
+            </Grid.Root>
+        </div>
+    );
 };
-export default meta;
 
-type Story = StoryObj<typeof Icon>;
-
-export const Basic: Story = {
+export const CustomStyles = {
     args: {
-        name: 'close',
-        alt: 'close icon',
-    },
-};
-
-export const WithTooltip: Story = {
-    args: {
-        name: 'search',
-        title: 'description',
-    },
-};
-
-export const CustomStyles: Story = {
-    args: {
-        name: 'add',
-        title: 'description',
+        value: 'add',
         style: { border: '1px solid black' },
     },
+};
+
+export const WithTooltip = () => {
+    return (
+        <Tooltip.Root openDelay={0}>
+            <Tooltip.Trigger asChild>
+                <Icon value="info" />
+            </Tooltip.Trigger>
+            <Tooltip.Content>additional information</Tooltip.Content>
+        </Tooltip.Root>
+    );
+};
+
+export const WithButton = () => {
+    return (
+        <Popover.Root>
+            <Flex align="center">
+                <Text.Paragraph>Support cases</Text.Paragraph>
+                <Popover.Trigger asChild>
+                    <IconButton> info </IconButton>
+                </Popover.Trigger>
+            </Flex>
+            <Popover.Positioner>
+                <Popover.Content>
+                    <Popover.Title> Info </Popover.Title>
+                    <Popover.Description>
+                        This represents your entire organization`s supports cases. To view support
+                        cases that you have opened, visit the support center.
+                    </Popover.Description>
+                    <Toolbar.Root isSmall>
+                        <Popover.CloseTrigger asChild>
+                            <Button>Close</Button>
+                        </Popover.CloseTrigger>
+                        <Button variant="accent">Learn more</Button>
+                    </Toolbar.Root>
+                </Popover.Content>
+            </Popover.Positioner>
+        </Popover.Root>
+    );
 };
