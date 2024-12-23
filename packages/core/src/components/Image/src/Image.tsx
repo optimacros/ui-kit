08ea48c0e2053746bf991e8e2f235563d8d4c11e
gzhuravlev <g.zhuravlev@optisoft.net>
2024-12-23 19:21:42 +0300

    feat(image): created

---
+++ packages/core/src/components/Image/src/Image.tsx
@@ -0,0 +1,73 @@
+import { createReactApiStateContext, forward, styled } from '@optimacros-ui/store';
+import { ComponentProps } from 'react';
+import * as avatar from '@zag-js/avatar';
+
+export const { Api, useApi, RootProvider, useSelector, useProxySelector } =
+    createReactApiStateContext({
+        id: 'image',
+        machine: avatar,
+        connect(api, { state, send }, machine) {
+            return {
+                ...api,
+                getRootProps() {
+                    return {
+                        ...api.getRootProps(),
+                        'data-scope': 'image',
+                    };
+                },
+                getImageProps() {
+                    return {
+                        ...api.getImageProps(),
+                        'data-scope': 'image',
+                    };
+                },
+                getFallbackProps() {
+                    return {
+                        ...api.getFallbackProps(),
+                        'data-scope': 'image',
+                    };
+                },
+            };
+        },
+    });
+
+export const Root = forward<
+    {
+        ratio: string;
+    } & ComponentProps<typeof RootProvider>,
+    'div'
+>(({ children, style, className, ratio, ...props }, ref) => {
+    return (
+        <RootProvider {...props}>
+            {(api) => (
+                <styled.div
+                    {...api.getRootProps()}
+                    ref={ref}
+                    style={style}
+                    className={className}
+                    data-aspect-ratio={ratio}
+                >
+                    {children}
+                </styled.div>
+            )}
+        </RootProvider>
+    );
+});
+
+export const Image = forward<{}, 'img'>((props, ref) => {
+    const api = useApi();
+
+    return <styled.img {...props} {...api.getImageProps()} ref={ref} />;
+});
+
+export const Avatar = forward<{}, 'img'>((props, ref) => {
+    const api = useApi();
+
+    return <styled.img {...props} {...api.getImageProps()} ref={ref} data-tag="avatar" />;
+});
+
+export const Fallback = forward<{}, 'div'>((props, ref) => {
+    const api = useApi();
+
+    return <styled.div {...props} {...api.getFallbackProps()} ref={ref} />;
+});
