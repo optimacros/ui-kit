7f0d291da10ce8746c73eeda803d13de5e3b40cb
JuliaJu93 <kashlewa.ju93@gmail.com>
2024-12-26 16:35:07 +0300

    chore(navigation): added adapter

---
+++ packages/internal/src/Navigation/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Orientation } from '@optimacros-ui/utils';
+import { Navigation as NavigationComponent } from '@optimacros-ui/navigation';
+
+type NavigationTheme = {
+    horizontal: string;
+    vertical: string;
+};
+
+type BaseNavigationProps = {
+    type?: Orientation;
+    theme: NavigationTheme;
+    className?: string;
+    wrap?: boolean;
+};
+
+export interface Props extends Omit<BaseNavigationProps, 'theme'> {
+    theme?: Partial<NavigationTheme>;
+}
+
+export type NavigationProps = React.PropsWithChildren<Props>;
+
+export const Navigation: React.FC<NavigationProps> = ({ type, children }) => {
+    return <NavigationComponent.Root orientation={type}>{children}</NavigationComponent.Root>;
+};
