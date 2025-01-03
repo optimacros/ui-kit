ea5c786483ab238be3db8480a46a9425387a9788
Front-start <a.chepusov@optisoft.net>
2025-01-03 21:43:31 +0300

    refactor(core): toolbar

--- packages/core/src/components/Toolbar/src/Toolbar.tsx
+++ packages/core/src/components/Toolbar/src/Toolbar.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { forward, styled } from '@optimacros-ui/store';
+import { Align } from '@optimacros-ui/utils';
 
-export type ToolbarProps = React.PropsWithChildren;
+export interface ToolbarProps {
+    align?: Align;
+    isSmall?: boolean;
+}
 
-export const Root = forward<ToolbarProps, 'div'>((props, ref) => (
-    <styled.div {...props} ref={ref} data-scope="toolbar" data-part="root" />
-));
+export const Root = forward<React.PropsWithChildren<ToolbarProps>, 'div'>(
+    ({ isSmall = false, ...rest }, ref) => (
+        <styled.div
+            {...rest}
+            ref={ref}
+            data-scope="toolbar"
+            data-part="root"
+            // TODO data-size=small|default? какой у нас подход?
+            // не должны ли мы align передавать как data-align? rightInRow же не будет работать
+            data-small={isSmall}
+        />
+    ),
+);
