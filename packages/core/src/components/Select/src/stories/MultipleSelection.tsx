208c94127040eb12ff0cb9e6f7ad3a00da13453d
gzhuravlev <g.zhuravlev@optisoft.net>
2025-01-13 23:01:16 +0300

    feat(test): added tests

--- packages/core/src/components/Select/src/stories/MultipleSelection.tsx
+++ packages/core/src/components/Select/src/stories/MultipleSelection.tsx
@@ -2,9 +2,12 @@ import { Select } from '@optimacros-ui/select';
 import { Icon } from '@optimacros-ui/icon';
 import { Field } from '@optimacros-ui/field';
 import { ControlTemplate } from './components';
+import { StoryObj } from '@storybook/react';
+import { within, expect, waitFor, fireEvent } from '@storybook/test';
+import { fn } from '@storybook/test';
 
-export const MultipleSelection = {
-    args: { multiple: true },
+export const MultipleSelection: StoryObj = {
+    args: { multiple: true, onValueChange: fn() },
     render: (props) => {
         return (
             <ControlTemplate {...props}>
@@ -25,5 +28,38 @@ export const MultipleSelection = {
                 </Select.Api>
             </ControlTemplate>
         );
+    },
+    play: async ({ step, canvasElement }) => {
+        const canvas = within(canvasElement);
+        const content = within(document.body).getByTestId('content');
+        const list = within(document.body).getByTestId('list');
+
+        const trigger = canvas.getByTestId('trigger');
+
+        await step('open', async () => {
+            await fireEvent.click(trigger);
+
+            await waitFor(() => {
+                expect(content).toHaveAttribute('data-state', 'open');
+            });
+        });
+
+        await step('choose', async () => {
+            for (const item of list.childNodes) {
+                await fireEvent.click(item);
+
+                await waitFor(() => {
+                    expect(item).toHaveAttribute('aria-selected', 'true');
+                });
+            }
+        });
+
+        await step('close', async () => {
+            fireEvent.click(trigger);
+
+            await waitFor(() => {
+                expect(content).toHaveAttribute('data-state', 'closed');
+            });
+        });
     },
 };
