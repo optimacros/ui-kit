308b0ad36c4ff731a2232f099c81f79fc9287ddf
JuliaJu93 <kashlewa.ju93@gmail.com>
2025-01-17 17:51:45 +0300

    chore(select): added adapter

--- packages/internal/src/SelectBox/index.tsx
+++ packages/internal/src/SelectBox/index.tsx
@@ -1,15 +1,62 @@
 import React from 'react';
-import { without } from 'lodash';
-import type { SelectBoxTheme, SelectBoxProps as Props } from './SelectBox';
 import type { InputTheme } from '../Input';
 import { Field, Icon, IconButton, Select } from '@optimacros-ui/kit';
 import { Flex } from '@optimacros-ui/flex';
 
-export interface SelectBoxProps extends Omit<Props, 'theme'> {
+export type SelectBoxTheme = {
+    active: string;
+    disabled: string;
+    dropdown: string;
+    error: string;
+    errored: string;
+    field: string;
+    label: string;
+    required: string;
+    selected: string;
+    focused: string;
+    templateValue: string;
+    up: string;
+    value: string;
+    values: string;
+    Title: string;
+};
+
+type SelectBoxSourceLabel = keyof SelectBoxProps['source'][number];
+type SelectBoxSourceValue = SelectBoxProps['source'][number][SelectBoxSourceLabel];
+
+export interface SelectBoxProps {
     theme?: Partial<SelectBoxTheme & InputTheme>;
     multiSelect?: boolean;
-    onChange?: (value: string | number | (string | number)[], event?: React.SyntheticEvent) => void;
+    onChange?: (value: string | number | (string | number)[]) => void;
+    options: any[];
+    source: { [key: string]: any }[];
+    labelKey?: string;
+    valueKey?: string;
+    name?: string;
+    label?: string;
+    value?: SelectBoxSourceValue | SelectBoxSourceValue[];
+    allowBlank?: boolean;
+    auto?: boolean;
+    className?: string;
+    disabled?: boolean;
+    error?: string | null;
+    onBlur?: (event: React.SyntheticEvent) => void;
+    onClick?: (event: React.MouseEvent) => void;
+    onFocus?: React.FocusEventHandler<HTMLDivElement>;
+    required?: boolean;
+    template?: (item: SelectBoxProps['source'][number] | undefined) => React.ReactNode;
 }
+
+const getStatus = (disabled: boolean, error: boolean) => {
+    switch (true) {
+        case disabled:
+            return 'readonly';
+        case error:
+            return 'error';
+        default:
+            return 'default';
+    }
+};
 
 export const SelectBox: React.FC<SelectBoxProps> = ({
     label,
@@ -18,49 +65,39 @@ export const SelectBox: React.FC<SelectBoxProps> = ({
     theme: customTheme = {},
     value,
     source,
+    options,
     onChange,
+    required,
+    error,
     ...otherProps
 }) => {
-    // Функция обработки изменения значения
-    const handleChange = (newValue: string | number, event: React.SyntheticEvent) => {
-        let updatedValue: SelectBoxProps['value'] = newValue;
+    const handleChange = (newValue) => {
+        const updatedValueArr = newValue.value;
 
         if (multiSelect && Array.isArray(value)) {
-            updatedValue = [...value, newValue];
-        }
-
-        if (onChange) {
-            onChange(updatedValue, event);
-        }
-    };
-
-    // Функция удаления элемента
-    const handleDelete = (deletedValue: string | number) => {
-        if (!Array.isArray(value)) {
-            return;
-        }
-
-        const newValue = without(value, deletedValue);
-
-        if (onChange) {
-            onChange(newValue);
+            onChange(updatedValueArr);
+        } else {
+            onChange(updatedValueArr[0]);
         }
     };
 
     return (
-        //     <SelectBoxComponent
-        //         auto={false}
-        //         {...otherProps}
-        //         source={filteredElements}
-        //         onChange={handleChange}
-        //     />
         <>
             {!multiSelect ? (
-                <Select.Root items={source} value={value} onValueChange={onChange} {...otherProps}>
+                <Select.Root
+                    items={source || options}
+                    value={value}
+                    onValueChange={handleChange}
+                    {...otherProps}
+                >
                     <Select.Control>
                         <Select.Api>
                             {(api) => (
-                                <Field.Root status={api.disabled ? 'readonly' : 'default'}>
+                                <Field.Root
+                                    status={getStatus(api.disabled, Boolean(error))}
+                                    required={required}
+                                    invalid={error}
+                                >
                                     <Select.Trigger>
                                         {label && (
                                             <Field.FloatingLabel>{label}</Field.FloatingLabel>
@@ -72,6 +109,7 @@ export const SelectBox: React.FC<SelectBoxProps> = ({
                                                 <Icon value={'arrow_drop_down'} />
                                             </Field.Icon>
                                         </Field.TriggerInput>
+                                        {error && <span>{error}</span>}
                                     </Select.Trigger>
                                 </Field.Root>
                             )}
@@ -82,8 +120,10 @@ export const SelectBox: React.FC<SelectBoxProps> = ({
                         <Select.Content>
                             <Select.List>
                                 {(item) => (
-                                    <Select.Item item={item} key={item.key}>
-                                        <Select.ItemLabel>{item.label}</Select.ItemLabel>
+                                    <Select.Item item={item} key={item.key || item.value}>
+                                        <Select.ItemLabel>
+                                            {item.label || item.title}
+                                        </Select.ItemLabel>
                                     </Select.Item>
                                 )}
                             </Select.List>
@@ -91,79 +131,79 @@ export const SelectBox: React.FC<SelectBoxProps> = ({
                     </Select.Positioner>
                 </Select.Root>
             ) : (
-                <>
-                    <Select.Root {...otherProps} items={source}>
-                        <Flex gap={5} direction="column">
-                            <Flex gap={3} wrap="wrap">
-                                Selected values:
-                                <Select.Api>
-                                    {(api) =>
-                                        api.value.map((value) => {
-                                            return (
-                                                <div className="bg-primary p-1.5 rounded-xs">
-                                                    {value}
-                                                    <Select.ItemDeleteTrigger
-                                                        item={{ value }}
-                                                        asChild
-                                                    >
-                                                        <IconButton
-                                                            size="sm"
-                                                            squared
-                                                            icon="close"
-                                                        />
-                                                    </Select.ItemDeleteTrigger>
-                                                </div>
-                                            );
-                                        })
-                                    }
-                                </Select.Api>
-                            </Flex>
-                            <Select.Control>
-                                <Select.Api>
-                                    {(api) => (
-                                        <Field.Root status={api.disabled ? 'readonly' : 'default'}>
-                                            <Field.TriggerInput
-                                                {...api.getTriggerProps()}
-                                                value={
-                                                    api.empty ? 'choose value' : api.valueAsString
-                                                }
-                                            >
-                                                <Field.Icon>
-                                                    <Icon value={'arrow_drop_down'} />
-                                                </Field.Icon>
-                                            </Field.TriggerInput>
-                                        </Field.Root>
-                                    )}
-                                </Select.Api>
-                            </Select.Control>
+                <Select.Root
+                    items={source || options}
+                    value={value}
+                    onValueChange={handleChange}
+                    multiple={true}
+                    {...otherProps}
+                >
+                    <Flex gap={5} direction="column">
+                        <Flex gap={3} wrap="wrap">
+                            Selected values:
+                            <Select.Api>
+                                {(api) =>
+                                    api.value.map((value, i) => {
+                                        return (
+                                            <div className="bg-primary p-1.5 rounded-xs" key={i}>
+                                                {value}
+                                                <Select.ItemDeleteTrigger item={{ value }} asChild>
+                                                    <IconButton size="sm" squared icon="close" />
+                                                </Select.ItemDeleteTrigger>
+                                            </div>
+                                        );
+                                    })
+                                }
+                            </Select.Api>
                         </Flex>
+                        <Select.Control>
+                            <Select.Api>
+                                {(api) => (
+                                    <Field.Root
+                                        status={getStatus(api.disabled, Boolean(error))}
+                                        required={required}
+                                    >
+                                        {label && (
+                                            <Field.FloatingLabel>{label}</Field.FloatingLabel>
+                                        )}
+                                        <Field.TriggerInput
+                                            {...api.getTriggerProps()}
+                                            value={api.empty ? 'choose value' : api.valueAsString}
+                                        >
+                                            <Field.Icon>
+                                                <Icon value={'arrow_drop_down'} />
+                                            </Field.Icon>
+                                        </Field.TriggerInput>
+                                        {error && <span>{error}</span>}
+                                    </Field.Root>
+                                )}
+                            </Select.Api>
+                        </Select.Control>
+                    </Flex>
 
-                        <Select.Positioner>
-                            <Select.Content>
-                                <Select.List>
-                                    {(item) => (
-                                        <Select.Item item={item} key={`select-${item.value}`}>
-                                            {({ selected }) => (
-                                                <>
-                                                    <div>
-                                                        {selected ? (
-                                                            <Icon value="check_box" />
-                                                        ) : (
-                                                            <Icon value="check_box_outline_blank" />
-                                                        )}
-                                                    </div>
-                                                    <Select.ItemLabel>
-                                                        {item.label}
-                                                    </Select.ItemLabel>
-                                                </>
-                                            )}
-                                        </Select.Item>
-                                    )}
-                                </Select.List>
-                            </Select.Content>
-                        </Select.Positioner>
-                    </Select.Root>
-                </>
+                    <Select.Positioner>
+                        <Select.Content>
+                            <Select.List>
+                                {(item) => (
+                                    <Select.Item item={item} key={`select-${item.value}`}>
+                                        {({ selected }) => (
+                                            <>
+                                                <div>
+                                                    {selected ? (
+                                                        <Icon value="check_box" />
+                                                    ) : (
+                                                        <Icon value="check_box_outline_blank" />
+                                                    )}
+                                                </div>
+                                                <Select.ItemLabel>{item.label}</Select.ItemLabel>
+                                            </>
+                                        )}
+                                    </Select.Item>
+                                )}
+                            </Select.List>
+                        </Select.Content>
+                    </Select.Positioner>
+                </Select.Root>
             )}
         </>
     );
