1fb251e0a0dbc9fdb77a9868b1337c86983837f2
JuliaJu93 <kashlewa.ju93@gmail.com>
2025-01-17 12:11:12 +0300

    fix(menu): fixed props

--- packages/internal/src/Menu/index.tsx
+++ packages/internal/src/Menu/index.tsx
@@ -5,37 +5,35 @@ interface Props {
     title: string;
     key: string;
     label?: string;
+    value?: string;
     onClick: (event: MouseEvent) => void;
     children: React.ReactNode;
 }
 
-export const MenuItem = (props: Props) => {
-    const { label, title, children, onClick, key, ...restProps } = props;
-    const content = label || title;
+export const MenuItem = ({ label, title, value, children, onClick, ...restProps }: Props) => {
     const generatedKey = useId();
 
     return (
         <div onClick={onClick}>
             <MenuComponent.Item
                 {...restProps}
-                valueText={content || children}
-                value={key || content || generatedKey}
-                key={key || content || generatedKey}
+                valueText={label || title || children}
+                value={value || generatedKey}
             />
         </div>
     );
 };
 
-export const SubMenu = ({ label, title, key, children }) => {
+export const SubMenu = ({ label, title, value, children }) => {
     const api = MenuComponent.useApi();
+    const generatedKey = useId();
 
     return (
         <MenuComponent.SubMenuItem
-            key={key}
             parent={api}
             item={{
-                value: label || title || children,
-                valueText: label || title,
+                value: value || generatedKey,
+                valueText: label || title || children,
                 closeOnSelect: true,
             }}
             closeOnSelect={false}
