c56beb26029abde6c9c6b53a3db3e48402c430de
Front-start <a.chepusov@optisoft.net>
2025-01-20 12:41:51 +0700

    feat(core/colorpicker): add exports

--- packages/core/src/components/ColorPicker/src/exports.ts
+++ packages/core/src/components/ColorPicker/src/exports.ts
@@ -1,3 +1,4 @@
 export * from './utils';
 export * from './ColorPicker';
 export * from './components';
+export * from './models';
--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -33,3 +33,4 @@ export * from '@optimacros-ui/tree-menu';
 export * from '@optimacros-ui/tree-view';
 export * from '@optimacros-ui/popover';
 export * from '@optimacros-ui/favourite';
+export * from '@optimacros-ui/color-picker';
