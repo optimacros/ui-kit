b045a5debd43fd1fe84a05bce9e8443264a83492
Front-start <a.chepusov@optisoft.net>
2025-01-21 19:42:38 +0700

    chore(lint-staged): improve pre-commit hook
    1 run biome on staged files only
    2 typecheck staged files

---
+++ scripts/type-check.mjs
@@ -0,0 +1,44 @@
+import ts from 'typescript';
+import path from 'path';
+
+function compile(fileName, options) {
+    const program = ts.createProgram(fileName, options);
+    const emitResult = program.emit();
+
+    const allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
+
+    allDiagnostics.forEach((diagnostic) => {
+        if (diagnostic.file) {
+            const { line, character } = ts.getLineAndCharacterOfPosition(
+                diagnostic.file,
+                diagnostic.start,
+            );
+
+            const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+
+            console.info(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
+        } else {
+            console.info(ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'));
+        }
+    });
+
+    const exitCode = allDiagnostics.length > 0 ? 1 : 0;
+    console.info(`Process exiting with code '${exitCode}'.`);
+    process.exit(exitCode);
+}
+
+process.argv.slice(2).forEach((file) => {
+    const configPath = ts.findConfigFile(file, ts.sys.fileExists, 'tsconfig.json');
+
+    // Read tsconfig.json file
+    const tsconfigFile = ts.readConfigFile(configPath, ts.sys.readFile);
+
+    // Resolve extends
+    const parsedTsconfig = ts.parseJsonConfigFileContent(
+        tsconfigFile.config,
+        ts.sys,
+        path.dirname(configPath),
+    );
+
+    compile([file], parsedTsconfig.options);
+});
