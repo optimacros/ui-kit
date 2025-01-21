2dda1a3548cfce388c6ad67fcd18e9c5f9eca8d8
JuliaJu93 <kashlewa.ju93@gmail.com>
2025-01-21 21:27:26 +0300

    chore(multiple): added adapter

---
+++ packages/internal/src/MultipleSelectBoxPanel/MultipleSelectBoxPanel.stories.tsx
@@ -0,0 +1,47 @@
+import { ArgTypes, Meta, StoryObj } from '@storybook/react';
+import { ReactNode, useState } from 'react';
+
+import { MultipleSelectBoxPanel } from './index';
+import { SelectBoxProps } from '../SelectBox';
+
+const argTypes: Partial<ArgTypes> = {};
+
+const meta: Meta<typeof MultipleSelectBoxPanel> = {
+    title: 'UI KIT Internal/MultipleSelectBoxPanel',
+    component: MultipleSelectBoxPanel,
+    argTypes,
+};
+export default meta;
+
+type Story = StoryObj<typeof MultipleSelectBoxPanel>;
+
+const source = [
+    { label: 'Newer first', value: 1 },
+    { label: 'Older first', value: 2 },
+    { label: 'No sort', value: 3 },
+];
+
+const Wrapper = ({ children }: { children: ReactNode }) => (
+    <div style={{ width: '200px' }}>{children}</div>
+);
+
+const Template: Story = {
+    render: ({ ...args }) => {
+        const [value, setValue] = useState<SelectBoxProps['value']>(1);
+
+        return <MultipleSelectBoxPanel {...args} value={value} onChange={(val) => setValue(val)} />;
+    },
+};
+
+export const Basic: Story = {
+    ...Template,
+    args: {
+        name: 'sort',
+        label: 'Sort',
+        source: source,
+    },
+    decorators: [
+        // eslint-disable-next-line new-cap
+        (Story) => <Wrapper>{Story()}</Wrapper>,
+    ],
+};
