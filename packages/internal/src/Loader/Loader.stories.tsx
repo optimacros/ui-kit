a6be304f075fc00fbf1424f8f9d05664754b0474
JuliaJu93 <kashlewa.ju93@gmail.com>
2025-01-23 14:56:06 +0300

    chore(progressbar): added progress bar

--- packages/internal/src/Loader/Loader.stories.tsx
+++ packages/internal/src/Loader/Loader.stories.tsx
@@ -1,6 +1,5 @@
 import { ArgTypes, Meta, StoryObj } from '@storybook/react';
-
-import { Loader } from './index';
+import { Loader } from '@optimacros-ui/kit-internal';
 
 const argTypes: Partial<ArgTypes> = {
     disabled: {
--- packages/internal/src/Loader/index.tsx
+++ packages/internal/src/Loader/index.tsx
@@ -62,3 +62,5 @@ export const Loader: LoaderProps = ({
         </LoaderComponent.Root>
     );
 };
+
+export const ProgressBar = Loader;
