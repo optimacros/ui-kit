f575706e4f74bcf2a813425337b51b36b7369e77
JuliaJu93 <kashlewa.ju93@gmail.com>
2025-02-07 15:18:40 +0300

    chore(tabs): added stories

--- packages/internal/src/Tabs/stories/index.ts
+++ packages/internal/src/Tabs/stories/index.ts
@@ -1,5 +1,7 @@
 export * from './Basic';
+export * from './Icons';
 export * from './Controlled';
 export * from './Draggable';
+export * from './Disabled';
 export * from './Scroll';
 export * from './TabHeader';
