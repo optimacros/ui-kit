73fcdfd662ec6e7bd845dbd8c56369b3a5068874
yyusupova <y.yusupova@optisoft.net>
2025-02-07 12:54:40 +0000

    UKF-514 Toolbar

--- packages/internal/src/Toolbar/Toolbar.stories.tsx
+++ packages/internal/src/Toolbar/Toolbar.stories.tsx
@@ -1,33 +1,30 @@
-//@ts-nocheck
-
-import { ArgTypes, Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { Toolbar } from './index';
 import { Button } from '@optimacros-ui/button';
-
-const argTypes: Partial<ArgTypes> = {
-    align: {
-        control: 'radio',
-        options: ['right', 'center', 'left', 'rightInRow'],
-        description: `Position of the toolbar`,
-        table: {
-            defaultValue: {
-                summary: 'left',
-            },
-            type: { summary: 'right | center | left | rightInRow' },
-        },
-    },
-    children: {
-        table: {
-            disable: true,
-        },
-    },
-};
 
 const meta: Meta<typeof Toolbar> = {
     title: 'UI Kit internal/Toolbar',
     component: Toolbar,
-    argTypes,
+    tags: ['autodocs'],
+    argTypes: {
+        align: {
+            control: { type: 'radio' },
+            options: ['left', 'right', 'center', 'rightInRow'],
+            defaultValue: 'left',
+            description: 'Alignment of toolbar items',
+        },
+    },
 };
+
+const content = [
+    <Button key="1" variant="accent">
+        button
+    </Button>,
+    <Button key="2" variant="primary">
+        another button
+    </Button>,
+];
+
 export default meta;
 
 type Story = StoryObj<typeof Toolbar>;
@@ -35,11 +32,34 @@ type Story = StoryObj<typeof Toolbar>;
 export const Basic: Story = {
     args: {
         align: 'left',
-        children: (
-            <>
-                <Button variant="accent">button</Button>
-                <Button variant="primary">another button</Button>
-            </>
-        ),
+        children: content,
+    },
+};
+
+export const LeftAligned: Story = {
+    args: {
+        align: 'left',
+        children: content,
+    },
+};
+
+export const RightAligned: Story = {
+    args: {
+        align: 'right',
+        children: content,
+    },
+};
+
+export const CenterAligned: Story = {
+    args: {
+        align: 'center',
+        children: content,
+    },
+};
+
+export const RightInRowAligned: Story = {
+    args: {
+        align: 'rightInRow',
+        children: content,
     },
 };
