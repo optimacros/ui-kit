01e042c44a4f82171a8fad864d6f399f048a1b1a
Front-start <a.chepusov@optisoft.net>
2025-02-12 00:41:40 +0700

    test(core/divider): refactor story, add test

--- packages/core/src/components/Divider/src/Divider.stories.tsx
+++ packages/core/src/components/Divider/src/Divider.stories.tsx
@@ -2,46 +2,72 @@ import { Flex } from '@optimacros-ui/flex';
 import { Text } from '@optimacros-ui/text';
 import { Icon } from '@optimacros-ui/icon';
 import { Orientation } from '@optimacros-ui/utils';
-import { Divider } from './Divider';
+import { Divider, DividerProps } from './Divider';
+import { ArgTypes, Meta, StoryObj } from '@storybook/react';
 
-export default {
+const argTypes: ArgTypes<DividerProps> = {
+    orientation: {
+        control: { type: 'radio' },
+        options: [Orientation.Horizontal, Orientation.Vertical],
+        description: 'Orientation',
+        table: {
+            type: { summary: `Orientation` },
+            defaultValue: { summary: `Orientation.Horizontal` },
+        },
+    },
+    fluid: {
+        control: 'boolean',
+        description: 'Whether component has 100% width/height',
+        table: {
+            defaultValue: { summary: `false` },
+        },
+    },
+    as: { table: { disable: true } },
+    asChild: { table: { disable: true } },
+};
+
+const meta: Meta<typeof Divider> = {
     title: 'UI Kit core/Divider',
     component: Divider,
-    tags: ['autodocs'],
-    argTypes: {
-        orientation: {
-            control: { type: 'radio' },
-            options: Orientation,
-        },
+    argTypes,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Divider>;
+
+export const Base: Story = {
+    args: { orientation: Orientation.Horizontal, fluid: false },
+};
+
+export const Vertical: Story = {
+    args: { orientation: Orientation.Vertical },
+    render: (props) => {
+        return (
+            <Flex gap="3" align="center">
+                <Icon value="format_align_left" />
+                <Icon value="format_align_center" />
+                <Icon value="format_align_right" />
+                <Divider {...props} />
+                <Icon value="format_bold" />
+            </Flex>
+        );
     },
 };
 
-export const Base = (props) => {
-    return <Divider {...props} />;
-};
-
-export const Vertical = (props) => {
-    return (
-        <Flex gap="3" align="center">
-            <Icon value="format_align_left" />
-            <Icon value="format_align_center" />
-            <Icon value="format_align_right" />
-            <Divider orientation={Orientation.Vertical} {...props} />
-            <Icon value="format_bold" />
-        </Flex>
-    );
-};
-
-export const Horizontal = (props) => {
-    return (
-        <div style={{ width: '200px' }}>
-            <Flex align="center" direction="column">
-                <Text.Paragraph>Full width variant below</Text.Paragraph>
-                <Divider orientation={Orientation.Horizontal} fluid {...props} />
-                <Text.Paragraph>Inset variant below</Text.Paragraph>
-                <Divider orientation={Orientation.Horizontal} fluid {...props} />
-                <Text.Paragraph>Middle variant below</Text.Paragraph>
-            </Flex>
-        </div>
-    );
+export const HorizontalFluid: Story = {
+    args: { orientation: Orientation.Horizontal, fluid: true },
+    render: (props) => {
+        return (
+            <div style={{ width: '200px' }}>
+                <Flex align="center" direction="column">
+                    <Text.Paragraph>Full width variant below</Text.Paragraph>
+                    <Divider {...props} />
+                    <Text.Paragraph>Inset variant below</Text.Paragraph>
+                    <Divider {...props} />
+                    <Text.Paragraph>Middle variant below</Text.Paragraph>
+                </Flex>
+            </div>
+        );
+    },
 };
--- packages/core/src/components/Divider/src/Divider.tsx
+++ packages/core/src/components/Divider/src/Divider.tsx
@@ -1,15 +1,33 @@
 import { forward, styled } from '@optimacros-ui/store';
 import { Orientation } from '@optimacros-ui/utils';
+import { ComponentProps } from 'react';
 
-export const Divider = forward<{ orientation?: Orientation; fluid?: boolean }, 'hr'>(
-    ({ orientation = Orientation.Horizontal, fluid, ...rest }, ref) => (
-        <styled.hr
-            {...rest}
-            data-scope="divider"
-            data-part="root"
-            data-orientation={orientation}
-            data-fluid={fluid}
-            ref={ref}
-        />
-    ),
+interface Props {
+    orientation?: Orientation;
+    fluid?: boolean;
+}
+
+export type DividerProps = ComponentProps<typeof Divider>;
+
+export const Divider = forward<Props, 'hr'>(
+    ({ orientation = Orientation.Horizontal, fluid, ...rest }, ref) => {
+        const getProps = () => {
+            const p = {
+                ...rest,
+                'data-scope': 'divider',
+                'data-part': 'root',
+                'data-orientation': orientation,
+                ref,
+            };
+
+            if (fluid) {
+                p['data-fluid'] = true;
+            }
+
+            return p;
+        };
+
+        return <styled.hr {...getProps()} />;
+    },
+    { memoize: true, displayName: 'Divider' },
 );
