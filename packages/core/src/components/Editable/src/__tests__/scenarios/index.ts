8610bbe46f1a156f23276a1c0ccff8b860570eda
Front-start <a.chepusov@optisoft.net>
2025-02-11 22:28:50 +0700

    test(core/editable): add tests

---
+++ packages/core/src/components/Editable/src/__tests__/scenarios/index.ts
@@ -0,0 +1,5 @@
+export * from './placeholder';
+export * from './states';
+export * from './maxLength';
+export * from './autoResize';
+export * from './basic';
--- packages/core/src/components/Editable/src/stories/States.tsx
+++ packages/core/src/components/Editable/src/stories/States.tsx
@@ -1,103 +1,101 @@
 import { Editable } from '@optimacros-ui/editable';
 import { Flex } from '@optimacros-ui/flex';
+import { EditableProps } from '../Editable';
 
-export const States = {
-    args: { 'edit.controlled': true, edit: true },
-    render: (props) => {
-        return (
-            <Flex direction="column" gap={4}>
-                <Flex direction="column" gap={2}>
-                    <Editable.RootProvider {...props} invalid value="invalid">
-                        {(api) => (
-                            <Editable.Root>
-                                <Editable.Label>Invalid</Editable.Label>
-                                <Editable.Area>
-                                    <Editable.Input />
-                                    <Editable.Preview />
-                                </Editable.Area>
+export const States = (props: EditableProps) => {
+    return (
+        <Flex direction="column" gap={4}>
+            <Flex direction="column" gap={2}>
+                <Editable.RootProvider {...props} invalid value="invalid">
+                    {(api) => (
+                        <Editable.Root data-testid="root">
+                            <Editable.Label>Invalid</Editable.Label>
+                            <Editable.Area>
+                                <Editable.Input />
+                                <Editable.Preview />
+                            </Editable.Area>
 
-                                {!api.editing ? (
-                                    <Editable.EditTrigger>Edit</Editable.EditTrigger>
-                                ) : (
-                                    <Flex align="center" gap={2}>
-                                        <Editable.SubmitTrigger>Save</Editable.SubmitTrigger>
-                                        <Editable.CancelTrigger>Cancel</Editable.CancelTrigger>
-                                    </Flex>
-                                )}
-                            </Editable.Root>
-                        )}
-                    </Editable.RootProvider>
-                </Flex>
-
-                <Flex direction="column" gap={2}>
-                    <Editable.RootProvider {...props} disabled value="disabled">
-                        {(api) => (
-                            <Editable.Root>
-                                <Editable.Label>Disabled</Editable.Label>
-                                <Editable.Area>
-                                    <Editable.Input />
-                                    <Editable.Preview />
-                                </Editable.Area>
-
-                                {!api.editing ? (
-                                    <Editable.EditTrigger>Edit</Editable.EditTrigger>
-                                ) : (
-                                    <Flex align="center" gap={2}>
-                                        <Editable.SubmitTrigger>Save</Editable.SubmitTrigger>
-                                        <Editable.CancelTrigger>Cancel</Editable.CancelTrigger>
-                                    </Flex>
-                                )}
-                            </Editable.Root>
-                        )}
-                    </Editable.RootProvider>
-                </Flex>
-
-                <Flex direction="column" gap={2}>
-                    <Editable.RootProvider {...props} readOnly value="readOnly">
-                        {(api) => (
-                            <Editable.Root>
-                                <Editable.Label>Read only</Editable.Label>
-                                <Editable.Area>
-                                    <Editable.Input />
-                                    <Editable.Preview />
-                                </Editable.Area>
-
-                                {!api.editing ? (
-                                    <Editable.EditTrigger>Edit</Editable.EditTrigger>
-                                ) : (
-                                    <Flex align="center" gap={2}>
-                                        <Editable.SubmitTrigger>Save</Editable.SubmitTrigger>
-                                        <Editable.CancelTrigger>Cancel</Editable.CancelTrigger>
-                                    </Flex>
-                                )}
-                            </Editable.Root>
-                        )}
-                    </Editable.RootProvider>
-                </Flex>
-
-                <Flex direction="column" gap={2}>
-                    <Editable.RootProvider {...props} required value="required">
-                        {(api) => (
-                            <Editable.Root>
-                                <Editable.Label>Required</Editable.Label>
-                                <Editable.Area>
-                                    <Editable.Input />
-                                    <Editable.Preview />
-                                </Editable.Area>
-
-                                {!api.editing ? (
-                                    <Editable.EditTrigger>Edit</Editable.EditTrigger>
-                                ) : (
-                                    <Flex align="center" gap={2}>
-                                        <Editable.SubmitTrigger>Save</Editable.SubmitTrigger>
-                                        <Editable.CancelTrigger>Cancel</Editable.CancelTrigger>
-                                    </Flex>
-                                )}
-                            </Editable.Root>
-                        )}
-                    </Editable.RootProvider>
-                </Flex>
+                            {!api.editing ? (
+                                <Editable.EditTrigger>Edit</Editable.EditTrigger>
+                            ) : (
+                                <Flex align="center" gap={2}>
+                                    <Editable.SubmitTrigger>Save</Editable.SubmitTrigger>
+                                    <Editable.CancelTrigger>Cancel</Editable.CancelTrigger>
+                                </Flex>
+                            )}
+                        </Editable.Root>
+                    )}
+                </Editable.RootProvider>
             </Flex>
-        );
-    },
+
+            <Flex direction="column" gap={2}>
+                <Editable.RootProvider {...props} disabled value="disabled">
+                    {(api) => (
+                        <Editable.Root data-testid="root">
+                            <Editable.Label>Disabled</Editable.Label>
+                            <Editable.Area>
+                                <Editable.Input />
+                                <Editable.Preview />
+                            </Editable.Area>
+
+                            {!api.editing ? (
+                                <Editable.EditTrigger>Edit</Editable.EditTrigger>
+                            ) : (
+                                <Flex align="center" gap={2}>
+                                    <Editable.SubmitTrigger>Save</Editable.SubmitTrigger>
+                                    <Editable.CancelTrigger>Cancel</Editable.CancelTrigger>
+                                </Flex>
+                            )}
+                        </Editable.Root>
+                    )}
+                </Editable.RootProvider>
+            </Flex>
+
+            <Flex direction="column" gap={2}>
+                <Editable.RootProvider {...props} readOnly value="readOnly">
+                    {(api) => (
+                        <Editable.Root data-testid="root">
+                            <Editable.Label>Read only</Editable.Label>
+                            <Editable.Area>
+                                <Editable.Input />
+                                <Editable.Preview />
+                            </Editable.Area>
+
+                            {!api.editing ? (
+                                <Editable.EditTrigger>Edit</Editable.EditTrigger>
+                            ) : (
+                                <Flex align="center" gap={2}>
+                                    <Editable.SubmitTrigger>Save</Editable.SubmitTrigger>
+                                    <Editable.CancelTrigger>Cancel</Editable.CancelTrigger>
+                                </Flex>
+                            )}
+                        </Editable.Root>
+                    )}
+                </Editable.RootProvider>
+            </Flex>
+
+            <Flex direction="column" gap={2}>
+                <Editable.RootProvider {...props} required value="required">
+                    {(api) => (
+                        <Editable.Root data-testid="root">
+                            <Editable.Label>Required</Editable.Label>
+                            <Editable.Area>
+                                <Editable.Input />
+                                <Editable.Preview />
+                            </Editable.Area>
+
+                            {!api.editing ? (
+                                <Editable.EditTrigger>Edit</Editable.EditTrigger>
+                            ) : (
+                                <Flex align="center" gap={2}>
+                                    <Editable.SubmitTrigger>Save</Editable.SubmitTrigger>
+                                    <Editable.CancelTrigger>Cancel</Editable.CancelTrigger>
+                                </Flex>
+                            )}
+                        </Editable.Root>
+                    )}
+                </Editable.RootProvider>
+            </Flex>
+        </Flex>
+    );
 };
