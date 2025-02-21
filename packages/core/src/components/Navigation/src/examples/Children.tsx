d2e5538bef8ea9ba0beaf9a4f3bfdaff8442f9e0
Front-start <a.chepusov@optisoft.net>
2025-02-21 15:49:39 +0700

    test(core/navigation): update story

---
+++ packages/core/src/components/Navigation/src/examples/Children.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@optimacros-ui/button';
+
+export const Children = (
+    <>
+        <Button> Portfolio </Button>
+        <Button> About </Button>
+        <Button> Menu </Button>
+        <Button> Location </Button>
+        <Button> Contacts </Button>
+    </>
+);
