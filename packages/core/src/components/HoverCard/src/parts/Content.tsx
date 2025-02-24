7f50ae558ffbf45795437656d146899882b9374e
JuliaJu93 <kashlewa.ju93@gmail.com>
2025-02-24 20:54:04 +0300

    chore(rating-group): cherry-pick

---
+++ packages/core/src/components/HoverCard/src/parts/Content.tsx
@@ -0,0 +1,8 @@
+import { forward, styled } from '@optimacros-ui/store';
+import { useApi } from '../state';
+
+export const Content = forward<{}, 'div'>((props, ref) => {
+    const api = useApi();
+
+    return <styled.div {...props} {...api.getContentProps()} ref={ref} />;
+});
