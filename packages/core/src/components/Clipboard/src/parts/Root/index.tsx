1f30e5e021bcf09184d3a464fbe4325078285751
JuliaJu93 <kashlewa.ju93@gmail.com>
2025-02-24 20:54:04 +0300

    chore(clipbord): started

---
+++ packages/core/src/components/Clipboard/src/parts/Root/index.tsx
@@ -0,0 +1,7 @@
+import { ComponentProps } from 'react';
+
+import { RootProvider, Api } from '../../state';
+
+export { RootProvider as Root, Api };
+
+export type RootProps = ComponentProps<typeof RootProvider>;
