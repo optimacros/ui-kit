423ac066595ea2bbc279de24f734d970c7fe37f3
JuliaJu93 <kashlewa.ju93@gmail.com>
2025-02-26 00:30:21 +0300

    chore(hover-card): fixed

--- packages/core/src/components/HoverCard/src/parts/Content.tsx
+++ packages/core/src/components/HoverCard/src/parts/Content.tsx
@@ -1,8 +1,8 @@
 import { forward, styled } from '@optimacros-ui/store';
 import { useApi } from '../state';
 
-export const Content = forward<{}, 'div'>(({ size, ...rest }, ref) => {
+export const Content = forward<{}, 'div'>((props, ref) => {
     const api = useApi();
 
-    return <styled.div {...rest} {...api.getContentProps()} ref={ref} />;
+    return <styled.div {...props} {...api.getContentProps()} ref={ref} />;
 });
