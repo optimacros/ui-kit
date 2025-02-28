674a551261e3c408212dc47f1949cde846af0f50
gzhuravlev <g.zhuravlev@optisoft.net>
2025-03-01 02:10:10 +0300

    fix(menu): fixed stories

--- packages/core/src/components/Menu/src/Menu.stories.tsx
+++ packages/core/src/components/Menu/src/Menu.stories.tsx
@@ -147,46 +147,18 @@ export const OrientationExample: Story = {
                         </div>
                     )}
                 </Menu.Api>
-                <Menu.Api>
-                    {(api) => (
-                        <Menu.Positioner>
-                            <Menu.Content size="sm" data-testid="menu-content">
-                                <Menu.List>
-                                    {menuItems.map((v) => (
-                                        <Menu.Item {...v} key={v.value}>
-                                            {v.valueText}
-                                        </Menu.Item>
-                                    ))}
-                                    <Menu.SubMenuItem
-                                        // @ts-ignore
-                                        parent={api}
-                                        item={{
-                                            value: 'sub-menu-nested',
-                                            valueText: 'nested',
-                                            closeOnSelect: true,
-                                        }}
-                                        positioning={{
-                                            fitViewport: false,
-                                            overlap: false,
-                                        }}
-                                    >
-                                        <Menu.SubMenuPositioner>
-                                            <Menu.Content data-testid="sub-menu-content">
-                                                <Menu.List>
-                                                    {menuItems.slice(0, 5).map((v) => (
-                                                        <Menu.Item {...v} key={v.value + 'sub-sub'}>
-                                                            {v.valueText}
-                                                        </Menu.Item>
-                                                    ))}
-                                                </Menu.List>
-                                            </Menu.Content>
-                                        </Menu.SubMenuPositioner>
-                                    </Menu.SubMenuItem>
-                                </Menu.List>
-                            </Menu.Content>
-                        </Menu.Positioner>
-                    )}
-                </Menu.Api>
+
+                <Menu.Positioner>
+                    <Menu.Content size="sm" data-testid="menu-content">
+                        <Menu.List>
+                            {menuItems.map((v) => (
+                                <Menu.Item {...v} key={v.value}>
+                                    {v.valueText}
+                                </Menu.Item>
+                            ))}
+                        </Menu.List>
+                    </Menu.Content>
+                </Menu.Positioner>
             </Menu.Root>
         );
     },
