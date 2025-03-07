6a028deb61b168c66e77cba40643b7592906836a
gzhuravlev <g.zhuravlev@optisoft.net>
2025-03-07 16:03:53 +0300

    fix(tooltip): stories

--- packages/core/src/components/Tooltip/src/Tooltip.stories.tsx
+++ packages/core/src/components/Tooltip/src/Tooltip.stories.tsx
@@ -68,11 +68,11 @@ export const Basic: StoryObj<typeof Tooltip.Root> = {
 };
 
 export const LongText: StoryObj<typeof Tooltip.Root> = {
-    ...Base,
     args: {
         children:
             'The disease was diagnosed as pseudopseudohypoparathyroidism, a rare genetic disorder',
     },
+    render: examples.Basic,
 };
 
 export const Positioning: StoryObj<typeof Tooltip.Root> = {
