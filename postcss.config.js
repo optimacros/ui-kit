c1aa89493b4e29d1058dddd007f8fe1724369f07
Front-start <a.chepusov@optisoft.net>
2025-03-26 21:08:51 +0700

    chore(styling): add postcss plugin to convert rem to px

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,13 @@ module.exports = {
         'postcss-import': {
             path: ['src'],
         },
+        'postcss-rem-to-responsive-pixel': {
+            rootValue: 16,
+            propList: ['*'],
+            transformUnit: 'px',
+            replace: true,
+            mediaQuery: true,
+        },
         // 'postcss-apply': {},
         // 'postcss-preset-env': {
         //     features: {
