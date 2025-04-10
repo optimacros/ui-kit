e1dd147bdb26b682e28bc4f3a2f9f3ea5e378b76
Front-start <a.chepusov@optisoft.net>
2025-04-11 00:31:24 +0700

    feat(internal/icon): add text node into svg

--- packages/internal/src/Icon/index.tsx
+++ packages/internal/src/Icon/index.tsx
@@ -1,7 +1,7 @@
 import { Icon as BaseIcon, type IconProps as BaseProps } from '@optimacros-ui/icon';
 import { hasIn } from 'lodash-es';
 import { isObject } from '@optimacros-ui/utils';
-import { ComponentProps } from 'react';
+import { ComponentProps, useLayoutEffect, useRef } from 'react';
 
 import './styles.css';
 
@@ -10,13 +10,43 @@ export type IconProps = Omit<ComponentProps<typeof BaseIcon>, 'value'> & {
 };
 
 export const Icon = ({ value, ...rest }: IconProps) => {
+    const ref = useRef<HTMLElement>(null);
+
+    useLayoutEffect(() => {
+        if (ref.current.nodeName !== 'svg') {
+            return;
+        }
+
+        let valueString = null;
+
+        if (isObject(value) && hasIn(value, 'name')) {
+            // @ts-ignore
+            valueString = value.name;
+        } else if (typeof value === 'string') {
+            valueString = value;
+        }
+
+        if (!valueString) {
+            return;
+        }
+
+        ref.current.setAttribute('data-react-toolbox', 'font-icon');
+
+        const title = document.createElement('title');
+        title.textContent = valueString;
+
+        ref.current.append(title);
+    }, [ref.current]);
+
     if (isObject(value) && hasIn(value, 'name')) {
         //@ts-ignore
-        return <BaseIcon value={value.name} {...value} {...rest} data-style-tag="internal" />;
+        return (
+            <BaseIcon value={value.name} {...value} {...rest} data-style-tag="internal" ref={ref} />
+        );
     }
 
     //@ts-ignore
-    return <BaseIcon value={value} {...rest} data-style-tag="internal" />;
+    return <BaseIcon value={value} {...rest} data-style-tag="internal" ref={ref} />;
 };
 
 export const WSIcon = ({ value, ...rest }: IconProps) => {
