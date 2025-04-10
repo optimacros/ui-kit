42f92e9b5d66aff22c9ea35dff67f9130890de35
gzhuravlev <g.zhuravlev@optisoft.net>
2025-04-10 17:35:04 +0300

    fix(icons): fix

--- packages/internal/src/Icon/index.tsx
+++ packages/internal/src/Icon/index.tsx
@@ -1,6 +1,7 @@
 import { Icon as BaseIcon, type IconProps as BaseProps } from '@optimacros-ui/icon';
 import { hasIn } from 'lodash-es';
 import { isObject } from '@optimacros-ui/utils';
+import { ComponentProps } from 'react';
 
 export type IconProps = Omit<ComponentProps<typeof BaseIcon>, 'value'> & {
     value: BaseProps['value'] | { name: string; fill: string; opacity: number };
@@ -13,28 +14,9 @@ export const Icon = ({ value, ...rest }: IconProps) => {
     }
 
     //@ts-ignore
-
     return <BaseIcon value={value} {...rest} />;
 };
 
-import { startsWith, kebabCase } from '@optimacros-ui/utils';
-import { ComponentProps } from 'react';
-
-// value может прийти в виде { name: string, fill?: string, opacity?: string }
-export const iconValueParser = (value: any) => {
-    if (typeof value === 'object' && Object.hasOwn(value, 'name')) {
-        if (startsWith(value.name, 'icon')) {
-            return kebabCase(value.name.replace('icon', ''));
-        }
-
-        return value.name;
-    }
-
-    return value;
-};
-
-export const WSIcon = ({ value: valueProp, ...rest }: IconProps) => {
-    const value = iconValueParser(valueProp);
-
+export const WSIcon = ({ value, ...rest }: IconProps) => {
     return <Icon width="15" height="15" value={value} {...rest} />;
 };
