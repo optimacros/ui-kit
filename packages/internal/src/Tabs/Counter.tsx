53059ded558d25b6d467e91fe539de07fc6900b4
Front-start <a.chepusov@optisoft.net>
2025-04-16 23:17:17 +0700

    feat: add testid tabs

--- packages/internal/src/Tabs/Counter.tsx
+++ packages/internal/src/Tabs/Counter.tsx
@@ -8,11 +8,11 @@ interface Props extends Pick<TabExtended, 'counter' | 'maxCounter'> {
 
 export const Counter = memo<Props>(({ counter, className, maxCounter = Infinity }) => {
     if (counter === 0) {
-        return <Text.Span className={className} />;
+        return <Text.Span className={className} data-testid="tabs-counter" />;
     }
 
     return (
-        <Text.Span className={className}>
+        <Text.Span className={className} data-testid="tabs-counter">
             {counter <= maxCounter ? counter : `${maxCounter}+`}
         </Text.Span>
     );
